f9e984487b02ad2acc6273ea52f1b2119f81cf83
ryan <ball.ryan.rb@gmail.com>
2021-06-07 15:56:22 -0600

    adds RESTful routes and CRUD actions

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,39 @@
 class PlacesController < ApplicationController
+  def index
+    render json: Place.all.order(:id)
+  end
+
+  def create
+    place = Place.new(
+      name: params[:name],
+      address: params[:address],
+    )
+    if place.save
+      render json: place
+    else
+      render json: { errors: place.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    place = Place.find(params[:id])
+    render json: place 
+  end
+
+  def update
+    place = Place.find(params[:id])
+    place.name = params[:name] || place.name
+    place.address = params[:address] || place.address
+    if place.save
+      render json: place
+    else
+      render json: { errors: place.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    place = Place.find(params[:id])
+    place.destroy
+    render json: { message: "Place successfully destroyed!"}
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Place.create!(name: "Raku", address: "5030 Spring Mountain Rd, Las Vegas, NV 89146")
+Place.create!(name: "U.S. Thai Cafe", address: "5228 W 25th Ave, Edgewater, CO 80214")
+Place.create!(name: "Just BE Kitchen", address: "2364 15th St, Denver, CO 80202")
+Place.create!(name: "Sushi Den", address: "1487 S Pearl St, Denver, CO 80210")
